a55e39419e09d4b0f21c91ac176aabe3fa982216
paul <pluquec@unsa.edu.pe>
2020-10-22 22:38:07 +0000

    ejercicio 2, completo, (return incluido)

--- problema02.pl
+++ problema02.pl
@@ -4,13 +4,20 @@ $i = 0;
 
 while($num = <STDIN>){
   chomp ($num);
-  $numbers[$i] = 1* $num;
+  $numbers[$i] =  $num;
   $i++;
 }
 
-foreach $valor (@numbers){
-  print " $valor \n";
+$max = mayor(@numbers);
+print "El numero mayor es: $max\n";
+## ------------------------ Subrutinas ---------------------------------
+sub mayor{
+  $max = $_[0];
+  foreach $valor (@_){
+    if($max < $valor){
+      $max = $valor;
+    }
+  }
+  return $max;
 }
-
-
 
